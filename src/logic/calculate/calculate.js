a5cdf381e29d7d7f437eb66dacade9d4a2cdcc6d
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-05-16 01:22:58 -0300

    Update Calculate

--- src/logic/calculate/calculate.js
+++ src/logic/calculate/calculate.js
@@ -1,5 +1,12 @@
-const calculate = () => {
-
-}
+const calculate = (button, state) => {
+    return state;
+};
 
 export default calculate;
+
+export const State = {
+    current: "",
+    operand: 0,
+    operator: null,
+    isNextClear: false
+};
