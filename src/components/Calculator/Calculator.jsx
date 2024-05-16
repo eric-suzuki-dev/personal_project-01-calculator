0bbedbaacb782e52543034763b415b4863950397
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-05-16 01:23:33 -0300

    Update Calculator: useState

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+import calculate from "../../logic/calculate/calculate.js";
 import BtnPanel from "../BtnPanel/BtnPanel.jsx";
 import Display from "../Display/Display.jsx";
 
 const Calculator = () => {
+const [state, setState] = useState({
+    current: "0",
+    operand: 0,
+    operator: null,
+    isNextClear: false
+});
 
     const btnHandler = (code) =>{
-        console.log(code);
-    }
+     const nextState = calculate(code, state)
+     setState(nextState);
+    };
     return (
         <div>
             <Display />
