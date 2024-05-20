ecae254238e4865bb378e8aaadd0378e67bc16d3
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-05-19 23:12:26 -0300

    feat. Design

--- src/components/BtnPanel/BtnPanel.jsx
+++ src/components/BtnPanel/BtnPanel.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
 import './BtnPanel.css';
 
 const BtnPanel = ({ btnHandler }) => {
@@ -11,19 +10,6 @@ const BtnPanel = ({ btnHandler }) => {
                 <button onClick={() => btnHandler('%')}>%</button>
                 <button onClick={() => btnHandler('/')}>/</button>
             </div>
-=======
-import "./BtnPanel.css";
-
-const BtnPanel = ({ btnHandler }) => {
-  return (
-    <div className='btnPanel'>
-      <div>
-        <button onClick={() => btnHandler("7")}>7</button>
-        <button onClick={() => btnHandler("8")}>8</button>
-        <button onClick={() => btnHandler("9")}>9</button>
-        <button onClick={() => btnHandler("AC")}>AC</button>
-      </div>
->>>>>>> 26f34cb4b228b21da625fcabaf6f9e60f8a4f7dc
 
             <div>
                 <button onClick={() => btnHandler('7')}>7</button>
